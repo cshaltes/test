42d7135be749ccad8a53c69940436d8f68a11beb
Christian <mxchump@yahoo.com>
2021-10-07 11:50:38 -0700

    for real this time

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-class index extends Component {
-    render() {
-        return(
-            <div className="index">
-                <Button color="primary">Button</Button>{''}
-            </div>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-        );
-    }
-}
-
-export default Index;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
