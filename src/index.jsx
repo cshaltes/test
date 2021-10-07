cfa3dd8e0ae58182b8dc5fc2de6705246c20b04a
Christian <mxchump@yahoo.com>
2021-10-07 10:44:52 -0700

    maybework

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+class index extends Component {
+    render() {
+        return(
+            <div className="index">
+                <Button color="primary">Button</Button>{''}
+            </div>
+
+        );
+    }
+}
+
+export default Index;
