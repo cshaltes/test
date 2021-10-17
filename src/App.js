8608e193a66d0b65d311bff5e44fab1be78cd9fa
Christian <mxchump@yahoo.com>
2021-10-17 01:56:35 -0700

    new

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Row,
+    Col,
+    Jumbotron,
+    Button
+} from 'reactstrap';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Button color="danger">Danger!</Button>
+            </div>
+        );
+    }
+}
+
+export default App;
